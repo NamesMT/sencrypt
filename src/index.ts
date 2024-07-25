db76de0a8996851df962124c43d17c8137539908
NamesMT <dangquoctrung123@gmail.com>
2024-07-25 14:57:31 +0000

    docs: comment on types re-export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { SEncrypt } from './SEncrypt'
 export type { SEncryptStorageInterface, SEncryptEncrypterInterface } from './SEncrypt'
 
+// Re-exporting used types
 export type {
   SHashStorageInterface,
   SHash,
